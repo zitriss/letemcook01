711cd0175d9490393c4baa86d8d0f3a27e13a64c
nathou54545 <patate.licorne@outlook.fr>
2024-11-22 16:00:09 +0100

    Test changement de tempo
    En appuyant sur espace, le tempo passe en x2. On voit bien que le fichier MIDI et la musique sont toujours synchronisés

--- letemcook01godot/scripts/main_test.gd
+++ letemcook01godot/scripts/main_test.gd
@@ -3,6 +3,10 @@ extends Node
 func _ready() -> void:
 	$MidiAudioPlayer.get_node("MidiPlayer").note.connect(on_note)
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		$MidiAudioPlayer.get_node("AudioStreamPlayer").pitch_scale = 2
+
 func on_note(event, track) -> void:
 	if (event['subtype'] == MIDI_MESSAGE_NOTE_ON): # note on
 		print("Played note " + str(event['note']))
